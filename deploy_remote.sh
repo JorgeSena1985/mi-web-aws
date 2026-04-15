d9a824250d5ebe1c54e67d2a8fcb1d91e65f1e4b
jsenaris <jorgesss85@hotmail.com>
2026-04-15 19:47:36 +0200

    auto deploy

---
+++ deploy_remote.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+IP="52.213.89.124"
+KEY="~/mi-clave.pem"
+
+echo "📤 Subiendo cambios a GitHub..."
+git add .
+git commit -m "auto deploy"
+git push
+
+echo "🌐 Ejecutando deploy en servidor..."
+ssh -i $KEY ubuntu@$IP << EOF
+cd ~/apps/mi-web-aws
+./deploy_server.sh
+EOF
+
+echo "✅ Deploy completo 🚀"
