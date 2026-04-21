09fa08676b51a45602f127b37d3d283f943696e3
JorgeSena1985 <muchodetodo2025@outlook.es>
2026-04-21 13:40:57 +0200

    Añadiendoclave y carpeta correcta

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 IP="34.241.203.199"
-KEY= $HOME/my-key.pem
+KEY="$HOME/my-key.pem"
 
 echo "🚀 Subiendo web a todo pastilla..."
 scp -i $KEY -r . ubuntu@$IP:/home/ubuntu/mi-web-aws
 
 echo "🔧 Actualizando servidor..."
 ssh -i $KEY ubuntu@$IP << EOF
-sudo cp -r /home/ubuntu/web/* /var/www/html/
+sudo cp -r /home/ubuntu/mi-web-aws/* /var/www/html/
 sudo systemctl restart apache2
 EOF
 
